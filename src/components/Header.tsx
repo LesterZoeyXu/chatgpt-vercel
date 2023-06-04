9d04d4ec236fb70376bea7d6fe6534202b26770c
LesterXu <68421135+LesterZoeyXu@users.noreply.github.com>
2023-06-04 12:11:54 +0800

    Update Header.tsx
    1.修改chatgpt 这几个字改为欢迎使用， AI助手

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,11 +61,11 @@ export default function Header() {
             fallback={
               <>
                 <span class="text-transparent font-extrabold bg-clip-text bg-gradient-to-r dark:from-yellow-300 from-yellow-600 dark:to-red-700 to-red-700 mr-1">
-                  ChatGPT
+                  欢迎使用
                 </span>
 
                 <span class="ml-1 font-extrabold text-slate-7 dark:text-slate">
-                  Vercel
+                  AI助手
                 </span>
 
                 <a
